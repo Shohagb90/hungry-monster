53db6fd89c025b162261fc0fa3b6b6e731a8790c
Shohag Miah <shohagmiah314@gmail.com>
2021-02-09 21:53:24 +0600

    adding something

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const renderFoodInfo = (food) => {
     foodDetailsDiv.innerHTML = `
     <img class="img-fluid rounded mb-4" src="${food.strMealThumb}" alt="">
     <h4>${food.strMeal}</h4>
+    <h5>Ingredients:</h5>
     <ul class="list-unstyled mb-0">
     ${ingredients.map((ingredient) => `<li><i class="icon-check icons"></i>${ingredient}</li>`).join('')}
     </ul>
